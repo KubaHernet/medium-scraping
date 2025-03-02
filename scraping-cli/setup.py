07553c8b5da6d162f1ba13a1e63e5b99fad282f5
KubaHernet <jakub.hernet@gmail.com>
2025-03-02 16:54:56 +0100

    add scraping to scraping-cli package

--- scraping-cli/setup.py
+++ scraping-cli/setup.py
@@ -1,12 +1,18 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 with open('../scraping/requirements.txt', 'r', encoding='utf-8') as f:
     requirements = f.read().split('\n')
+    
+packages=find_packages('../scraping')
     
 setup(name='scraping-cli',
     version='0.0.1',
     install_requires=requirements,
     py_modules=['main'],
+    packages=packages,
+    package_dir={
+        'scraping': '../scraping/scraping'
+    },
     entry_points={
         'console_scripts': [
             'scrape = main:default'
